8ecef9340d896c1965188061c74fd09eec4bdbd5
dianess <analystdiane@gmail.com>
2019-12-06 10:53:53 -0700

    begin app.js

---
+++ D3_data_journalism/js/app.js
@@ -0,0 +1,15 @@
+//Define SVG area with hard-coded dimensions
+var svgWidth = 960;
+var svgHeight = 620;
+
+// Define the chart's margins as an object
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 200,
+    left: 100
+  };
+
+// Define dimensions of the chart area
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom; 
