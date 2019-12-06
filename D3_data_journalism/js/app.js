73fa5ee671fb0abfad666e2a63cf11c7660ebf15
dianess <analystdiane@gmail.com>
2019-12-06 11:08:22 -0700

    append group, set initial parameters

--- D3_data_journalism/js/app.js
+++ D3_data_journalism/js/app.js
@@ -21,3 +21,11 @@ var chart = d3.select("#scatter").append("div").classed("chart", true);
 var svg = chart.append("svg")
     .attr("width", svgWidth)
     .attr("height", svgHeight);
+
+// Append a group area with it's area inside the margins set above
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);  
+    
+// Set initial parameters
+var chosenXAxis = "poverty";
+var chosenYAxis = "healthcare";    
